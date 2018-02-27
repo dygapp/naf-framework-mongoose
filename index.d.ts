f502020220a1156ad210b6978900e0256a831313
dygapp <dygapp@qq.com>
2018-02-27 16:41:56 +0800

    0.0.41

--- index.d.ts
+++ index.d.ts
@@ -1,95 +1,96 @@
 import { Context } from "egg";
 
-/**
+declare namespace Services {
+  /**
  * NafService is a wrapper class for mongoose model
  */
-declare class NafModel { // tslint:disable-line
-  /**
-   * 构造函数
-   * @param ctx context对象 
-   * @param model mongoose model对象
-   */
-  constructor(ctx: Context, model: any);
+  declare class NafModel { // tslint:disable-line
+    /**
+     * 构造函数
+     * @param ctx context对象 
+     * @param model mongoose model对象
+     */
+    constructor(ctx: Context, model: any);
+
+    /**
+     * 原始的Mongoose model对象
+     */
+    model: any;
+
+    /**
+     * 插入数据
+     * @param data 数据对象
+     * @param model mongoose model对象 
+     */
+    _create(data: any);
+
+    _findById(_id: any);
+
+    _find(conditions: Object, projection: Object, options: Object);
+
+    _findOne(conditions: Object, projection: Object, options: Object);
+
+    _remove(conditions: Object);
+
+    _findOneAndUpdate(conditions: Object, update: Object, options: Object);
+
+    _findByIdAndUpdate(_id: any, update: Object, options: Object);
+
+    _update(conditions: Object, update: Object, options: Object);
+
+    _count(conditions: Object);
+
+  }
 
   /**
-   * 原始的Mongoose model对象
+   * NafService is a base service class that can be extended,
+   * it's extending from {@link Service},
    */
-  model: any;
+  export class NafService {
+    /**
+     * 构造函数
+     * @param ctx context对象 
+     * @param name service名称
+     */
+    constructor(ctx: Context, name: String);
+
+    /**
+     * 租户ID，用于多租户系统
+     */
+    tenant: string;
+
+    /** 
+     * 生成Id，sequence名用service的name
+     */
+    nextId(): any;
+
+    _model(model: any): NafModel;
+
+    /**
+     * 插入数据
+     * @param data 数据对象
+     * @param model mongoose model对象 
+     */
+    _create(data: Object, model: any);
+
+    _findById(_id: string, model: any);
+
+    _find(conditions: Object, projection: Object, options: Object, model: any);
+
+    _findOne(conditions: Object, projection: Object, options: Object, model: any);
+
+    _remove(conditions: Object, model: any);
+
+    _findOneAndUpdate(conditions: Object, update: Object, options: Object, model: any);
+
+    _update(conditions: Object, update: Object, options: Object, model: any);
+
+    _count(conditions: Object, model: any);
+  }
 
   /**
-   * 插入数据
-   * @param data 数据对象
-   * @param model mongoose model对象 
+   * CrudService is extending from {@link NafService} ,
    */
-  _create(data: any);
-
-  _findById(_id: any);
-
-  _find(conditions: Object, projection: Object, options: Object);
-
-  _findOne(conditions: Object, projection: Object, options: Object);
-
-  _remove(conditions: Object);
-
-  _findOneAndUpdate(conditions: Object, update: Object, options: Object);
-
-  _findByIdAndUpdate(_id: any, update: Object, options: Object);
-
-  _update(conditions: Object, update: Object, options: Object);
-
-  _count(conditions: Object);
-
+  export class CrudService extends NafService { }
 }
-
-/**
- * NafService is a base service class that can be extended,
- * it's extending from {@link Service},
- */
-export class NafService { 
-  /**
-   * 构造函数
-   * @param ctx context对象 
-   * @param name service名称
-   */
-  constructor(ctx: Context, name: String);
-
-  /**
-   * 租户ID，用于多租户系统
-   */
-  tenant: string;
-
-  /** 
-   * 生成Id，sequence名用service的name
-   */
-  nextId(): any;
-
-  _model(model: any): NafModel;
-
-  /**
-   * 插入数据
-   * @param data 数据对象
-   * @param model mongoose model对象 
-   */
-  _create(data: Object, model: any);
-
-  _findById(_id: string, model: any);
-
-  _find(conditions: Object, projection: Object, options: Object, model: any);
-
-  _findOne(conditions: Object, projection: Object, options: Object, model: any);
-
-  _remove(conditions: Object, model: any);
-
-  _findOneAndUpdate(conditions: Object, update: Object, options: Object, model: any);
-
-  _update(conditions: Object, update: Object, options: Object, model: any);
-
-  _count(conditions: Object, model: any);
-}
-
-/**
- * CrudService is extending from {@link NafService} ,
- */
-export class CrudService extends NafService { }
-
 export as namespace Naf;
--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 'use strict';
 
 module.exports = require('./lib/framework.js');
-// module.exports.service = require('./lib/service');
+module.exports.service = require('./lib/service');
 module.exports.controller = require('./lib/controller');
 
-module.exports.service = {
-  ...require('./lib/service/naf-service'),
-  /**
- * @member Naf#CrudService
- * @since 0.0.1
- */
-  CrudService: require('./lib/service/crud-service'),
-};
+module.exports.Services = require('./lib/service');
