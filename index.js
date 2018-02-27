b9e0f8b7b36ddc6e86a94e3ee751d32c94032335
dygapp <dygapp@qq.com>
2018-02-27 15:46:18 +0800

    0.0.39

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ module.exports = require('./lib/framework.js');
 module.exports.service = require('./lib/service');
 module.exports.controller = require('./lib/controller');
 
-/**
- * @member Naf#NafService
- * @since 0.0.1
- */
-exports.NafService = require('./lib/service').NafService;
+// exports.NafService = require('./lib/service').NafService;
 
-/**
- * @member Naf#CrudService
- * @since 0.0.1
- */
-exports.CrudService = require('./lib/service').CrudService;
+// /**
+//  * @member Naf#CrudService
+//  * @since 0.0.1
+//  */
+// exports.CrudService = require('./lib/service').CrudService;
