72fb437cb6668df49c49dbfb33525ac4be53231a
dygapp <dygapp@qq.com>
2018-02-27 15:55:27 +0800

    0.0.40

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 module.exports = require('./lib/framework.js');
-module.exports.service = require('./lib/service');
+// module.exports.service = require('./lib/service');
 module.exports.controller = require('./lib/controller');
 
-// exports.NafService = require('./lib/service').NafService;
-
-// /**
-//  * @member Naf#CrudService
-//  * @since 0.0.1
-//  */
-// exports.CrudService = require('./lib/service').CrudService;
+module.exports.service = {
+  ...require('./lib/service/naf-service'),
+  /**
+ * @member Naf#CrudService
+ * @since 0.0.1
+ */
+  CrudService: require('./lib/service/crud-service'),
+};
