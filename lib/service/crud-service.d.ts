43809b92f95e0d52ad20d06e9516e3fddb07d1a0
dygapp <dygapp@qq.com>
2018-08-31 18:36:31 +0800

    v0.3.14

---
+++ lib/service/crud-service.d.ts
@@ -0,0 +1,9 @@
+import * as Egg from 'egg';
+import * as mongoose from 'mongoose';
+
+import NafService from "./naf-service";
+
+/**
+ * CrudService is extending from {@link NafService} ,
+ */
+export class CrudService extends NafService { }
