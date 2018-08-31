4f4a57d82649784cedeb55c86983c1c7d15ec13c
dygapp <dygapp@qq.com>
2018-08-31 15:08:33 +0800

    rewrite some service mothod

--- app/service/seq.js
+++ app/service/seq.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const assert = require('assert');
 const Service = require('egg').Service;
+
 
 class SequenceService extends Service {
   async nextval(name) {
     const { ctx } = this;
-    const _id = `${ctx.tenant}_${name}`;
+    assert(ctx.model.Seq, 'Model Seq not found!');
+    assert(name, 'seq name must not empty!');
+
+    const _id = (ctx.tenant && `${ctx.tenant}_${name}`) || name;
     const { value } = await ctx.model.Seq.findByIdAndUpdate(_id,
       { $inc: { value: 1 } },
       { new: true, upsert: true }).exec();
