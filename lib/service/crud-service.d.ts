2132fc0a19e33185dda3ef4a9fd66aa468a1590d
dygapp <dygapp@qq.com>
2018-09-01 13:01:57 +0800

    v0.3.15 完善service类型定义

--- lib/service/crud-service.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Egg from 'egg';
-import * as mongoose from 'mongoose';
-
-import NafService from "./naf-service";
-
-/**
- * CrudService is extending from {@link NafService} ,
- */
-export class CrudService extends NafService { }
---
+++ lib/service/index.d.ts
@@ -0,0 +1,71 @@
+import * as EggApplication from 'egg';
+import * as mongoose from 'mongoose';
+
+declare module 'egg' {
+
+  type MongooseModels = {
+    [key: string]: mongoose.Model<any>
+  };
+
+  type MongooseSingleton = {
+    clients: Map<string, mongoose.Connection>,
+    get (id: string) : mongoose.Connection
+  };
+
+  type MongooseConfig = {
+    url: string,
+    options?: mongoose.ConnectionOptions
+  };
+
+  // extend app
+  interface Application {
+    mongooseDB: mongoose.Connection | MongooseSingleton;
+    mongoose: typeof mongoose;
+    model: MongooseModels;
+  }
+
+  // extend context
+  interface Context {
+    model: MongooseModels;
+
+    /**
+     * compose from ctx.query and ctx.request.body
+     */
+    requestparam: any;
+
+    /**
+     * 租户ID，用于多租户系统
+     */
+    tenant: string;
+
+    // 返回JSON结果
+    json(errcode: number, errmsg: string, data: object);
+    success(message: string, data: object);
+    fail(errcode: number, errmsg: string, details: any);
+    /**
+     * same to success(message: string, data: object);
+     */
+    ok(message: string, data: object);
+  }
+
+  // extend your config
+  interface EggAppConfig {
+    mongoose: {
+      url?: string,
+      options?: mongoose.ConnectionOptions,
+      client?: MongooseConfig,
+      clients?: {
+        [key: string]: MongooseConfig
+      }
+    };
+  }
+
+}
+
+import { NafService }  from './naf-service';
+export { NafService } from './naf-service';
+
+  /**
+ * CrudService is extending from {@link NafService} ,
+ */
+export class CrudService extends NafService { }
